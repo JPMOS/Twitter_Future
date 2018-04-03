f8fe234b97259bc8a303b235a299ff5398114672
Joel McGuire <joelmcguire@my.unt.edu>
2018-04-03 17:22:00 -0500

    Update TweetCollection.R
    Added "memory" of iterations so it keeps track of where you left off.

--- TweetCollection.R
+++ TweetCollection.R
@@ -8,97 +8,48 @@ setwd("/Users/jmcguire/Downloads/OU_Economics/Projects/")
 twitter_token <- readRDS("twitter_token.rds")
 
 
-big_tweets7 <- search_tweets( "lang:en", geocode = lookup_coords("usa"), n = 250000, 
-                             include_rts = FALSE, token = twitter_token, retryonratelimit = TRUE )
-
-tweets1 <- select(tweets1, text)
-tweets2 <- select(tweets2, text)
-tweets3 <- select(tweets3, text)
-tweets4 <- select(tweets4, text)
-tweets5 <- select(tweets5, text)
-tweets6 <- select(tweets6, text)
-
-
-tweets6 <- select(big_tweets6, text)
-tweets7 <- select(big_tweets7, text)
-
-tweets <- bind_rows(tweets1, tweets2, tweets3, tweets4, tweets5, tweets6, tweets7)
-nrow(tweets)
-head(tweets)
-
-big_tweets5 <- large_tweets1
-
-saveRDS(tweets, "2miltweetstext.rds")
-
-
-tweets <- `2miltweetstext`
-tweets1 <- select(tweets1, text)
-
-
 ####################
 setwd("/Users/jmcguire/Downloads/OU_Economics/Projects/")
-LIWC2015_Future_Cleaned <- read_csv("LIWC2015_Future_Cleaned.csv")
 
-head(LIWC2015_Future_Cleaned)
-
-TimeLexicon <- gather(LIWC2015_Future_Cleaned, key = "focus", value = "word")
-
-######################
-
-cleaned <- unnest_tokens(tweets, input = text, output = word) %>% 
-  anti_join(stop_words)
-
-frequency <- cleaned %>%
-  inner_join(filter(TimeLexicon, focus == "FocusFuture" | focus == "FocusPast")) %>%
-  group_by(word) %>%
-  count(word, sort = TRUE) 
-
-TimeLexicon %>% na.omit() %>% filter(focus == "FocusFuture" | focus == "FocusPast")
-head(TimeLexicon)
-
-words_to_filter_by <- frequency$word
-
-nrow(na.omit(TimeLexicon))
-omitNA
-nrow(frequency)
-
-
-low_frequency <- filter(TimeLexicon, focus != "FocusPresent" & !is.na(word)) %>% anti_join(frequency)
-
-View(low_frequency) # Choose what 36 not to add. 
-
-nrow(low_frequency) + nrow(frequency)
-
-View(frequency)
 
 ###########################################################################
 #                        A U T O M A T I O N 
 ###########################################################################
 
+#j <- 0
+#saveRDS(j, "count.Rds")
 
-
-
-
+################
+#    COLLECT
+################
 
 tweet_collection <- function(how_many_times) {
-  
-for (i in seq_along(1:how_many_times)) {
+  j <- readRDS("count.Rds")
+
+  for (i in seq_along(1:how_many_times)) {
  # idea we have j = 0
-  tweets <- search_tweets( "lang:en", geocode = lookup_coords("usa"), n = 250000, 
+  tweets <- search_tweets( "lang:en", geocode = lookup_coords("usa"), n = 100, 
                                           include_rts = FALSE, token = twitter_token, retryonratelimit = TRUE )
   
-  tweets <- select(tweets, screen_name, text, country, lang, geo_coords, coords_coords, bbox_coords) %>% 
+  tweets <- dplyr::select(tweets, screen_name, text, country, lang, geo_coords, coords_coords, bbox_coords) %>% 
     lat_lng() %>% 
     filter(!is.na(lng)) %>%
-    select(-geo_coords, -coords_coords, -bbox_coords) # %>% filter(lang == "en") 
-  # j += 1 ... then save j, and have it called at the next and then for name have it be j + i. 
-  saveRDS(tweets, paste("test", i + 40, ".rds", sep = ""))
+    dplyr::select(-geo_coords, -coords_coords, -bbox_coords) # %>% filter(lang == "en") 
+  
+  k <- ifelse(i == j, i, j )
+  saveRDS(tweets, paste("test", k, ".rds", sep = ""))
+  j <- j + 1 #... then save j, and have it called at the next and then for name have it be j + i. 
+  # if i == j then do nothing but if i < j then add i + j
+  saveRDS(j, "count.Rds")
   }
 }
 
-tweet_collection(5)
-.
+tweet_collection(2)
+tweet_collection(2)
 
+################
+#  CONCATENATE
+################
 
 read_tweets <- function(from_N, to_N) { # could I also say which... and say 2:8. Would that work? 
   tweetlist = list()
@@ -116,12 +67,6 @@ read_tweets <- function(from_N, to_N) { # could I also say which... and say 2:8.
 }
 
 tweets_reloaded <- read_tweets(from_N = 1, to_N = 7)
-
-saveRDS(tweets_reloaded, "geo_tweets1.rds")
-geo_tweets <- readRDS("geo_tweets1.rds")
-
-
-
 
 
 
